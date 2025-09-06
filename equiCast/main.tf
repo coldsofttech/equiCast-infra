8bc95bb1f87f3d80ca7b3d613d2f580fbbfb1abb
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-06 15:31:11 +0100

    Merge dev to main (#13)
    * Added S3 terraform module and tickers bucket creation
    
    * Removed infracost-gh-action
    
    * Fixed Infracost
    
    * Changed from terraform to equiCast
    
    * Enabled Infracost to run on both push and pull request
    
    * Added id-token permissions
    
    * Added working directory (#6)
    
    * Added working directory
    
    * Terraform Format Recursive
    
    * Added Terraform Init
    
    * Added backend=false
    
    * Fixed vars
    
    * Formatted Terraform files
    
    * Added tf-check.bat for local (#8)
    
    * Added working directory
    
    * Terraform Format Recursive
    
    * Added Terraform Init
    
    * Added backend=false
    
    * Fixed vars
    
    * Formatted Terraform files
    
    * Added tf-check.bat for local
    
    * Added fxpairs and ingestion buckets (#12)
    
    * Merge dev to main (#9)
    
    * Added S3 terraform module and tickers bucket creation
    
    * Removed infracost-gh-action
    
    * Fixed Infracost
    
    * Changed from terraform to equiCast
    
    * Enabled Infracost to run on both push and pull request
    
    * Added id-token permissions
    
    * Added working directory (#6)
    
    * Added working directory
    
    * Terraform Format Recursive
    
    * Added Terraform Init
    
    * Added backend=false
    
    * Fixed vars
    
    * Formatted Terraform files
    
    * Added tf-check.bat for local (#8)
    
    * Added working directory
    
    * Terraform Format Recursive
    
    * Added Terraform Init
    
    * Added backend=false
    
    * Fixed vars
    
    * Formatted Terraform files
    
    * Added tf-check.bat for local
    
    * Added fxpairs and ingestion buckets

--- equiCast/main.tf
+++ equiCast/main.tf
@@ -8,3 +8,17 @@ module "tickers_bucket" {
   bucket_name = "equicast-tickers"
   region      = "eu-west-1"
 }
+
+# S3 bucket for storing fx pairs data (fxpairs.json and fxpair_status.json)
+module "fxpairs_bucket" {
+  source      = "./modules/s3_bucket"
+  bucket_name = "equicast-fxpairs"
+  region      = "eu-west-1"
+}
+
+# S3 bucket for storing ingestion data (for example, stock prices, dividends, fundamentals, etc.)
+module "ingestion_bucket" {
+  source      = "./modules/s3_bucket"
+  bucket_name = "equicast-ingestion"
+  region      = "eu-west-1"
+}
