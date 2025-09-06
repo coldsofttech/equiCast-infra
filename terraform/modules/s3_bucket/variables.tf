b23dbb637eb2a5bc2f84bf2de4569ca045ed2e5d
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-06 13:49:08 +0100

    Added S3 terraform module and tickers bucket creation

---
+++ terraform/modules/s3_bucket/variables.tf
@@ -0,0 +1,46 @@
+variable "bucket_name" {
+    description = "Name of the S3 bucket"
+    type = string
+}
+
+variable "bucket_prefix" {
+    description = "Prefix to add to the S3 bucket name"
+    type = string
+    default = "equicast-"
+}
+
+variable "region" {
+    description = "AWS region code"
+    type = string
+    default = "eu-west-1"
+}
+
+variable "environment" {
+    description = "Value for the environment tag"
+    type = string
+    default = "production"
+}
+
+variable "enable_versioning" {
+    description = "Enable versioning for the S3 bucket"
+    type = bool
+    default = false
+}
+
+variable "block_public_access" {
+    description = "Block public access to the S3 bucket"
+    type = bool
+    default = true
+}
+
+variable "sse_algorithm" {
+    description = "Server-side encryption algorithm to use"
+    type = string
+    default = "AES256"
+}
+
+variable "tags" {
+    description = "A map of tags to add to all resources"
+    type = map(string)
+    default = {}
+}
