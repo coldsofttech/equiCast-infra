f5e979d0f04d7b179425b3d4747697217ceb92e0
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-06 14:52:54 +0100

    Added working directory (#6)
    * Added working directory
    
    * Terraform Format Recursive
    
    * Added Terraform Init
    
    * Added backend=false
    
    * Fixed vars
    
    * Formatted Terraform files

--- equiCast/main.tf
+++ equiCast/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-    region = "eu-west-1"
+  region = "eu-west-1"
 }
 
 # S3 bucket for storing ticker data (tickers.json and ticker_status.json)
 module "tickers_bucket" {
-    source = "./modules/s3_bucket"
-    bucket_name = "tickers"
-    region = "eu-west-1"
+  source      = "./modules/s3_bucket"
+  bucket_name = "tickers"
+  region      = "eu-west-1"
 }
