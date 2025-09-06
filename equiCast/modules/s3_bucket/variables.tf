c8dd23b73f9d12176077fd67c01f17bb38330c76
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-06 14:53:44 +0100

    Merge dev to main (#7)
    * Added S3 terraform module and tickers bucket creation
    
    * Removed infracost-gh-action
    
    * Fixed Infracost
    
    * Changed from terraform to equiCast
    
    * Enabled Infracost to run on both push and pull request
    
    * Added id-token permissions
    
    * Added working directory (#6)
    
    * Added working directory
    
    * Terraform Format Recursive
    
    * Added Terraform Init
    
    * Added backend=false
    
    * Fixed vars
    
    * Formatted Terraform files

--- equiCast/modules/s3_bucket/variables.tf
+++ equiCast/modules/s3_bucket/variables.tf
@@ -1,46 +1,46 @@
 variable "bucket_name" {
-    description = "Name of the S3 bucket"
-    type = string
+  description = "Name of the S3 bucket"
+  type        = string
 }
 
 variable "bucket_prefix" {
-    description = "Prefix to add to the S3 bucket name"
-    type = string
-    default = "equicast-"
+  description = "Prefix to add to the S3 bucket name"
+  type        = string
+  default     = "equicast-"
 }
 
 variable "region" {
-    description = "AWS region code"
-    type = string
-    default = "eu-west-1"
+  description = "AWS region code"
+  type        = string
+  default     = "eu-west-1"
 }
 
 variable "environment" {
-    description = "Value for the environment tag"
-    type = string
-    default = "production"
+  description = "Value for the environment tag"
+  type        = string
+  default     = "production"
 }
 
 variable "enable_versioning" {
-    description = "Enable versioning for the S3 bucket"
-    type = bool
-    default = false
+  description = "Enable versioning for the S3 bucket"
+  type        = bool
+  default     = false
 }
 
 variable "block_public_access" {
-    description = "Block public access to the S3 bucket"
-    type = bool
-    default = true
+  description = "Block public access to the S3 bucket"
+  type        = bool
+  default     = true
 }
 
 variable "sse_algorithm" {
-    description = "Server-side encryption algorithm to use"
-    type = string
-    default = "AES256"
+  description = "Server-side encryption algorithm to use"
+  type        = string
+  default     = "AES256"
 }
 
 variable "tags" {
-    description = "A map of tags to add to all resources"
-    type = map(string)
-    default = {}
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
 }
