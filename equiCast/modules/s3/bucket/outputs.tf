712420379cce303b8e124a1dcf362b66903f9180
coldsofttech <161084795+coldsofttech@users.noreply.github.com>
2025-09-13 11:00:48 +0100

    Added fixes
    1) Removed S3 website endpoint
    2) Removed dependency on .tfvars

--- equiCast/modules/s3/bucket/outputs.tf
+++ equiCast/modules/s3/bucket/outputs.tf
@@ -7,8 +7,3 @@ output "bucket_arn" {
   description = "The ARN of the S3 bucket"
   value       = aws_s3_bucket.this.arn
 }
-
-output "bucket_website_endpoint" {
-  description = "The website endpoint of the S3 bucket"
-  value       = aws_s3_bucket.this.website_endpoint
-}
